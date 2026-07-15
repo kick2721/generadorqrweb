7036b8c0a1cf5e7d3a7a40c5842751d1a1fac265
kick2721 <kick02721@gmail.com>
2026-07-15 15:11:05 +0100

    chore: add .temp/ to gitignore, remove tracked temp files

--- .temp/check-i18n.js
+++
@@ -1,7 +0,0 @@
-const fs = require("fs");
-const c = fs.readFileSync("src/lib/i18n.ts", "utf8");
-console.log("ES-dashboardSelected:", c.includes('dashboardSelected: "Seleccionados"'));
-console.log("ES-folderMoveTo:", c.includes('folderMoveTo: "Mover a carpeta"'));
-console.log("ES-folderExisting:", c.includes('folderExisting: "Carpetas existentes"'));
-console.log("EN-dashboardSelected count:", c.split('dashboardSelected: "Selected"').length - 1);
-console.log("Total dashboardSelected:", c.split("dashboardSelected").length - 1);
--- .temp/check-i18n4.js
+++
@@ -1,7 +0,0 @@
-const fs = require("fs");
-const c = fs.readFileSync("src/lib/i18n.ts", "utf8");
-const esMarker = 'folderCancel: "Cancelar selección"';
-const idx = c.indexOf(esMarker);
-// Check 200 chars before esMarker
-const before = c.substring(Math.max(0, idx - 200), idx);
-console.log("BEFORE folderCancel:", before);
